301c1f48bed30e8c3691d353551a73bb09a9f5cc
Siddhartha Pogula <durgampudi.g@gmail.com>
2022-09-08 05:25:09 -0400

    initial

---
+++ HsContactForm/pageObjects/HsSignUpForm.cs
@@ -0,0 +1,106 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HsContactForm.pageObjects
+{        public class HsSignUpForm
+        {
+            public HsSignUpForm(IWebDriver driver)
+            {
+                PageFactory.InitElements(driver, this);
+            }
+
+            [FindsBy(How = How.Id, Using = "firstname-input")]
+            private IWebElement firstName;
+
+            [FindsBy(How = How.Id, Using = "lastname-input")]
+            private IWebElement lastName;
+
+            [FindsBy(How = How.Id, Using = "email-input")]
+            private IWebElement userEmail;
+
+            [FindsBy(How = How.Id, Using = "international-country-code-select-input")]
+            private IWebElement country;
+
+            [FindsBy(How = How.Id, Using = "phone-input")]
+            private IWebElement phoneNumber;
+
+            [FindsBy(How = How.Id, Using = "company-input")]
+            private IWebElement company;
+
+            [FindsBy(How = How.Name, Using = "location")]
+            private IWebElement location;
+
+            [FindsBy(How = How.Id, Using = "message-input")]
+            private IWebElement message;
+
+            [FindsBy(How = How.Name, Using = "Submit")]
+            private IWebElement btnSubmit;
+
+            [FindsBy(How = How.XPath, Using = "//div[contains(@class,'thankyou-message')]//div")]
+            private IWebElement submissionSuccessForm;
+
+            public void enterFirstName(string frstName)
+            {
+                firstName.SendKeys(frstName);
+            }
+
+            public void enterLastName(string lstName)
+            {
+                lastName.SendKeys(lstName);
+            }
+
+            public void enterUserEmail(string email)
+            {
+                userEmail.SendKeys(email);
+            }
+
+            public void selectCountry(string countryName)
+            {
+                SelectElement selectElement = new SelectElement(country);
+                selectElement.SelectByText(countryName);
+            }
+
+            public void enterUserNumber(string number)
+            {
+                phoneNumber.SendKeys(number);
+            }
+
+            public void enterCompany(string com)
+            {
+                company.SendKeys(com);
+            }
+
+            public void enterLocation(string loc)
+            {
+                location.SendKeys(loc);
+            }
+
+            public void enterMessage(string mes)
+            {
+                message.SendKeys(mes);
+            }
+
+            public void clickSubmit()
+            {
+                btnSubmit.Click();
+            }
+
+            public string checkSubmissionSuccessForm()
+            {
+                return submissionSuccessForm.Text;
+            }
+
+            public string getCurrentDate(string format)
+            {
+                DateTime dateTime = DateTime.UtcNow.Date;
+                return dateTime.ToString(format);
+            }
+        }
+}
---
+++ HsContactForm/tests/SubmitHsSignUpForm.cs
@@ -0,0 +1,64 @@
+﻿using HsContactForm.pageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace HsContactForm.tests
+{
+    public class SubmitHsSignUpForm
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            //setup chrome driver
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void checkHsForm()
+        {
+            //lauches url
+            driver.Url = "https://share.hsforms.com/1-79TdZVpS9igd9mdtdea5w1khjs";
+            driver.Manage().Window.Maximize();
+
+            //set implicit wait for 10 secs
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //initiate practice form
+            HsSignUpForm form = new HsSignUpForm(driver);
+            string firstName = "TestFirstName";
+            string lastName = "TestLastName";
+            string message = firstName + " " + lastName + " " + form.getCurrentDate("dd/MM/yyyy");
+            //fill in the details and submit
+            form.enterFirstName(firstName);
+            form.enterLastName(lastName);
+            form.enterUserEmail("test@parall.ax");
+            form.selectCountry("United Kingdom");
+            form.enterUserNumber("07555555555");
+            form.enterCompany("Parallax");
+            form.enterLocation("Leeds");
+            form.enterMessage(message);
+            form.clickSubmit();
+            string actualText = form.checkSubmissionSuccessForm();
+            Console.WriteLine(actualText);
+            //check if submission form appeared
+            Assert.IsTrue(actualText.Contains("Thank you for contacting us!"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
